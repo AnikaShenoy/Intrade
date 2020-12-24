e44919f997a93e186d5bd4d3372e18636a4986ec
AnikaShenoy <anika.shenoy@mail.utoronto.ca>
2020-12-24 15:39:51 -0500

    added program for puts

--- using_B_S_model.py
+++ using_B_S_model.py
@@ -6,13 +6,17 @@ from sympy import init_printing
 init_printing()
 
 
-def call(S, X, T, r, sig):
+def black_scholes(S, X, T, r, sig, option):
     d1 = (np.log(S/X) + (r + ((sig**2)/2))*T)/(sig * (T**(1/2)))
     d2 = (np.log(S/X) + (r - ((sig**2)/2))*T)/(sig * (T**(1/2)))
-    call = S * N(d1) - X * np.exp(-r*T) * N(d2)
-    return call
+    res = 0
+    if option == "call":
+        res = S * N(d1) - X * np.exp(-r*T) * N(d2)
+    elif option == "put":
+        res = -S * N(-d1) + X * np.exp(-r*T) * N(-d2)
+    return res
 
 def N(d):
+    return si.norm.cdf(d, 0.0, 1.0)
     # return cumulative normal distribution for the standard normal distribution
     # probability that the number is <= d
-    return si.norm.cdf(d, 0.0, 1.0)
