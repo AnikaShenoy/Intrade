7939983f34de671db11f939bdfd85b570554b9e9
AnikaShenoy <anika.shenoy@mail.utoronto.ca>
2020-12-26 01:11:06 -0500

    removed unnecessary info

--- config.py
+++ config.py
@@ -2,7 +2,3 @@
 API_KEY = "PK8FF6XY34ZGG0R22M21"
 API_SECRET = "2coaEyS1HpkMDoUf76wVWHdgOpBVpyBiyvkkz6BU"
 APCA_API_BASE_URL = "https://paper-api.alpaca.markets"
-
-# Yahoo Finance API
-RAPIDAPI_KEY = "ccaaaa715amsh1b949a283bcf059p1d1041jsn0c923917d38e"
-RAPIDAPI_HOST = "apidojo-yahoo-finance-v1.p.rapidapi.com"
